8d13f2a74be223f294e042ea99568ca3d3c38fff
dotlinux26 <0206canh@gmail.com>
2024-11-02 01:53:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById('monut').addEventListener('click', function() {
+    const password = document.getElementById('mk').value;
+    const correctPassword = "101126"; // Mật khẩu đúng 
+
+    if (password === correctPassword) {
+        // mở khóa 
+        // lấy luôn kết quả của quay
+        const result = spinWheel();
+        document.getElementById('result').innerText = result;
+    } else {
+        alert('Mã bảo mật sai. Vui lòng thử lại.');
+    }
+});
+
+function spinWheel() {
+    const randomNum = Math.random();
+    if (randomNum < 0.4) {
+        return "Chúc mừng! Bạn đã trúng thưởng!";
+    } else {
+        return "Rất tiếc! Bạn đã hụt.";
+    }
+}
